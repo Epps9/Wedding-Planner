d4ea78f3add0005728d41f030fd13c9e387002cf
Ewa Witkowska <ewa.witkowska09@gmail.com>
2021-04-14 23:13:48 +0200

    Add Inspirations and Ref to Homepage

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Button} from '../../common/Button/Button';
 import { Link } from 'react-router-dom';
+import { FiArrowRightCircle, FiArrowLeftCircle } from 'react-icons/fi';
 
 import styles from './Homepage.module.scss';
 import cslx from 'clsx';
@@ -71,10 +72,46 @@ const Component = () => (
         </div>        
       </div>
     </section>
-    <section className={styles.three}></section>
-    <section className={styles.four}></section>
-    <section className={styles.five}></section>
+    <section id="inspirations" className={styles.four}>
+      <h1>Inspirations</h1>
+      <div className={styles.insp_boxes}>
+        <div className={cslx(styles.insp_box, styles.places)}>
+          <span></span>
+          <h2>Places</h2>
+        </div>
+        <div className={cslx(styles.insp_box, styles.dresses)}>
+          <span></span>
+          <h2>Dresses</h2>
+        </div>
+        <div className={cslx(styles.insp_box, styles.accessories)}>
+          <span></span>
+          <h2>Accessories</h2>
+        </div>
+      </div>
+    </section>
+    <section className={styles.five}>
+      <h1>References</h1>
+      <span className={styles.arrow}></span>
+      <div className={styles.testimonials}>
+        <FiArrowRightCircle className={cslx(styles.arrow, styles.arrow_right)}/>
+        <div className={styles.testimonial}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h3>Mat & Molly</h3>
+        </div>
+        <div className={styles.testimonial}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h3>Mat & Molly</h3>
+        </div>  
+        <div className={styles.testimonial}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h3>Mat & Molly</h3>
+        </div>
+        <FiArrowLeftCircle className={cslx(styles.arrow, styles.arrow_left)}/>
+      </div>
+      <span className={styles.arrow}></span>
+    </section>
     <section className={styles.six}></section>
+    <section className={styles.seven}></section>
   </div>
 );
 
