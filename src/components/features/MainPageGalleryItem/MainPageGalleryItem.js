adc0591a6c7582c6477c9f991ee1e904fc50da00
Ewa Witkowska <ewa.witkowska09@gmail.com>
2021-08-01 14:16:00 +0200

    start media queries for 768-1024

--- src/components/features/MainPageGalleryItem/MainPageGalleryItem.js
+++ src/components/features/MainPageGalleryItem/MainPageGalleryItem.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-import clsx from 'clsx';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
@@ -10,7 +9,7 @@ import clsx from 'clsx';
 import styles from './MainPageGalleryItem.module.scss';
 
 const Component = ({id, names, main_image}) => (
-  <div className={clsx(styles.image_box1, styles.box)}>
+  <div className={styles.box}>
     <span></span>
     <img src={main_image}></img>
     <h2>{names}</h2>
