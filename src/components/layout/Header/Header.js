c696c5816d56db01c1775da043640267e5494f24
Ewa Witkowska <ewa.witkowska09@gmail.com>
2021-08-19 23:27:47 +0200

    media queries corrected for Header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -7,17 +7,19 @@ import styles from './Header.module.scss';
 const Component = () => (
   <div className={styles.root}>
     <span></span>
-    <Link to="/" className={styles.logo_wrapper}>
-      <img className={styles.logo} src={'/static/images/marriage.png'}></img>
-      <p>Weddingo</p>
-    </Link>
-    <ul className={styles.menu}>
-      <li><Link to="/about">About us</Link></li>
-      <li><Link to="/offer">Offer</Link></li>
-      <li><Link to="/gallery">Gallery</Link></li>
-      <li><Link to="/inspirations">Inspirations</Link></li>
-      <li><Link to="/contact">Contact us</Link></li>
-    </ul>
+    <div className={styles.header_content}>
+      <Link to="/" className={styles.logo_wrapper}>
+        <img className={styles.logo} src={'/static/images/marriage.png'}></img>
+        <p>Weddingo</p>
+      </Link>
+      <ul className={styles.menu}>
+        <li><Link to="/about">About us</Link></li>
+        <li><Link to="/offer">Offer</Link></li>
+        <li><Link to="/gallery">Gallery</Link></li>
+        <li><Link to="/inspirations">Inspirations</Link></li>
+        <li><Link to="/contact">Contact us</Link></li>
+      </ul>
+    </div>
     <img className={styles.btn_main} src={'/static/images/down-arrow.png'}></img>
   </div>
 );
