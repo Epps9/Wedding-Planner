c0974666e79de154d880a4266a63ac3be8735dd2
Ewa Witkowska <ewa.witkowska09@gmail.com>
2021-09-30 23:29:03 +0200

    modify server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,17 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', galleryRoutes);
 
+/* API ERROR PAGES */
+app.use('/api', (req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+});
+
+/* REACT WEBSITE */
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
 
 /* MONGOOSE */
 mongoose.connect('mongodb+srv://Ewa:kodilla@cluster0.wsukt.mongodb.net/WeddingPlanner?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
